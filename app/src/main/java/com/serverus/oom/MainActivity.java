b032b7cbc5fc66cc49dbb9cc3f61e3225d86c92f
alvin joseph valdez <vlainlawliet@gmail.com>
2015-06-26 18:02:41 +0800

    initial commit with codes of prototype: splashscreen, drawer, images for home and event when you click the images in home

---
+++ app/src/main/java/com/serverus/oom/MainActivity.java
@@ -0,0 +1,126 @@
+package com.serverus.oom;
+
+import android.content.res.Configuration;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.NavigationView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Toolbar mToolBar;
+    private NavigationView mDrawer;
+    private ActionBarDrawerToggle mdrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private RelativeLayout digitalFrontier;
+    private RelativeLayout forwardThinkers;
+    private RelativeLayout preferedActions;
+
+    private LinearLayout innerParent;
+
+    public String viewVar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        digitalFrontier.setOnClickListener(this);
+        forwardThinkers.setOnClickListener(this);
+        preferedActions.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        mdrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void initViews(){
+        mToolBar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolBar);
+        mDrawer = (NavigationView) findViewById(R.id.main_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_parent);
+        mdrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                mToolBar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+
+        mDrawerLayout.setDrawerListener(mdrawerToggle);
+        // indicator based on whether the drawerlayout is in open or closed
+        mdrawerToggle.syncState();
+
+        digitalFrontier = (RelativeLayout) findViewById(R.id.digital_frontier);
+        forwardThinkers = (RelativeLayout) findViewById(R.id.forward_thinkers);
+        preferedActions = (RelativeLayout) findViewById(R.id.prefered_actions);
+
+        innerParent = (LinearLayout) findViewById(R.id.inner_parent);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.digital_frontier || v.getId() == R.id.forward_thinkers ||
+                v.getId() == R.id.prefered_actions ){
+
+            detailActivity(v);
+        }
+
+    }
+
+    private void detailActivity(View view){
+
+
+        switch (view.getId()){
+            case R.id.digital_frontier:
+                viewVar = "digital frontier";
+                break;
+            case R.id.forward_thinkers:
+                viewVar = "forward thinkers";
+                break;
+            case R.id.prefered_actions:
+
+                viewVar = "prefered actions";
+                break;
+        }
+
+        Snackbar.make(mDrawerLayout, viewVar , Snackbar.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/serverus/oom/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.serverus.oom;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by alvinvaldez on 6/24/15.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
