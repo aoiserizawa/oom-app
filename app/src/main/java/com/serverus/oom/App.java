5ab46b518ff6fe820c5b6d91ee95987d63084905
alvin joseph valdez <vlainlawliet@gmail.com>
2015-07-07 09:55:42 +0800

    added a class that will handle the connection for the Parse which will be activated in manifest in android:name=.App

---
+++ app/src/main/java/com/serverus/oom/App.java
@@ -0,0 +1,21 @@
+package com.serverus.oom;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by alvinvaldez on 7/6/15.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        Parse.enableLocalDatastore(this);
+//        Parse.initialize(this,
+//                "1vdHx98EkGTrOAqeLqnEcDl9rd3e9dZrKDcQyIzh",
+//                "MXhEirVd5kW8jWWEpN5WFSEB8cf5tawckCYG4MOh");
+    }
+}
